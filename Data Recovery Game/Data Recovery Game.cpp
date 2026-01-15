82e093aac5d5df7ed5a2739194dbf3df305e436f
StefanStefanov63 <s.p.stefanov@gmail.com>
2026-01-15 13:12:26 +0200

    reworked how words work, added semi fuctioning input and UI

--- Data Recovery Game/Data Recovery Game.cpp
+++ Data Recovery Game/Data Recovery Game.cpp
@@ -26,12 +26,14 @@ void printColored(const char* str, const int* color);
 void printColored(const char* str, const int* color,int start,int end);
 bool isUnpritable(char symbol);
 char* corruptText(const char* str, int* color,double randomiser);
-void getInputData(char input[],char text[], std::ifstream& ifs, double randomiser);
-void continueGameInputData(char corr[], int color[], std::ifstream& ifs, int errors);
+void getInputData(char input[],char text[], std::ifstream& ifs, double& randomiser);
+void continueGameInputData(char corr[], int color[], std::ifstream& ifs, int& errors);
 void start(char input[], char text[], std::ifstream& ifs, double& randomiser, char corr[], int color[], int& errors, int words[], int& wordsCount);
 void play(char input[], char text[], std::ifstream& ifs, double& randomiser, char corr[], int color[], int& errors, int words[], int& wordsCount);
 void chosenWord(char input[], char text[], std::ifstream& ifs, double& randomiser, char corr[], int color[], int& errors, int words[], int& wordsCount,int wordNumber);
 void chosenLetter(char input[], char text[], std::ifstream& ifs, double& randomiser, char corr[], int color[], int& errors, int words[], int& wordsCount, int wordNumber, int letterNumber);
+bool isPartOfWordCorr(const int* color, int start, int end);
+void changeCharTo(char sym);
 void saveGame();
 void exitGame();
 
@@ -41,8 +43,8 @@ int main()
 	/*char text[] = "The quick brown fox jumped over the lazy dog. ";
 	
 	std::cout << text;
-	double randomiser = 0.5,k=10;
-	while (k>0)
+	double randomiser = 0.5,k=10;*/
+	/*while (k>0)
 	{
 		int* color = createDinamicInt(strlenght(text));
 		char* corr = corruptText(text, color,randomiser);
@@ -53,13 +55,35 @@ int main()
 		k--;
 
 	}*/
-	//C:\Users\Admin\Desktop\input.txt
-	char input[BUFFER],text[BUFFER]="8cats ar bad f";
+	/*int* color = createDinamicInt(strlenght(text)), * words = nullptr, wordsCount = 0;;
+	char* corr = corruptText(text, color, randomiser);*/
+
+	/*C:\Users\Admin\Desktop\input.txt*/
+	char input[BUFFER], text[BUFFER];
 	std::ifstream ifs;
 	double randomiser = -1;
 	int* color = nullptr,* words = nullptr,errors=0,wordsCount=0;
 	char* corr = nullptr;
 	start(input, text, ifs, randomiser, corr, color, errors, words, wordsCount);
+	std::cout << "The absolute/relative path to a text file (.txt): \n";
+	std::cin.getline(input, BUFFER);
+	ifs.open(input);
+	if (!ifs.is_open()) {
+		std::cout << "Error!\n";
+		exit(0);
+	}
+	std::cout << input;
+
+
+	/*words = countWords(text, wordsCount);
+	printColored(text, color);
+	std::cout << wordsCount << '\n';
+	for (int i = 0; i < wordsCount; i++)
+	{
+		std::cout << words[2 * i] << " " << words[2 * i + 1] << " ";
+		printColored(text, color, words[2 * i], words[2 * i + 1]);
+		std::cout << '\n';
+	}*/
 	//
 
 	/*std::cout << std::bitset<8>('{') << "\n";
@@ -141,7 +165,12 @@ int countLetters(const char* str)
 
 int* countWords(const char* str,int& wordsCount)
 {
-	wordsCount = 0; int k = 0;
+	if (str == nullptr)
+		return nullptr;
+
+	wordsCount = 0;
+	int lenght = strlenght(str);
+	
 	if (!isBlank(str[0]))
 		wordsCount++;
 	for (int i = 1; str[i]; i++)
@@ -151,16 +180,26 @@ int* countWords(const char* str,int& wordsCount)
 			wordsCount++;
 		}
 	}
-	int* wordIndexes = createDinamicInt(wordsCount+1),n=1;
-	for (int i = 1; str[i]; i++)
+
+	int* wordIndexes = createDinamicInt(2*wordsCount),n=0;
+	if (!isBlank(str[0]))
+	{
+		wordIndexes[n] = 0;
+		n++;
+	}
+	for (int i = 1; i<lenght; i++)
 	{
 		if (isBlank(str[i-1]) && !isBlank(str[i]))
 		{
-			wordIndexes[n] == i;
+			wordIndexes[n] = i;
+			n++;
+		}
+		if((!isBlank(str[i]) && isBlank(str[i+1])))
+		{
+			wordIndexes[n] = i;
 			n++;
 		}
 	}
-	wordIndexes[n] = str[k];
 	
 	return wordIndexes;
 }
@@ -267,21 +306,18 @@ char* corruptText(const char* str, int* color, double randomiser)
 	return corr;
 }
 
-void getInputData(char input[],char text[], std::ifstream& ifs, double randomiser)
+void getInputData(char input[],char text[], std::ifstream& ifs, double& randomiser)
 {
-	//C:\Users\Admin\Desktop\input.txt
+	//C:/Users/Admin/Desktop/input.txt
+	system("cls");//
 	std::cout << "The absolute/relative path to a text file (.txt):\n";
 	std::cin.getline(input, BUFFER);
-	ifs>> input;
+	ifs.open(input);
 	if (!ifs.is_open()) {
-		std::cout << "Error!" << std::endl;
-		return;
+		std::cout << "Error!\n";
+		exit(0);
 	}
-
-	while (!ifs.eof())
-	{
-		ifs.getline(text, BUFFER);
-	}
+	ifs.getline(text, BUFFER);
 
 	ifs.close();
 	std::cout << "Real number (between 0 and 1) - to what degrer the text is corrupted:\n";
@@ -293,11 +329,11 @@ void getInputData(char input[],char text[], std::ifstream& ifs, double randomise
 	}
 }
 
-void continueGameInputData(char corr[], int color[], std::ifstream& ifs, int errors)
+void continueGameInputData(char corr[], int color[], std::ifstream& ifs, int& errors)
 {
 	if (!ifs.is_open()) {
-		std::cout << "Error!" << std::endl;
-		return;
+		std::cout << "Error!\n";
+		exit(0);
 	}
 
 	while (!ifs.eof()) {
@@ -319,6 +355,7 @@ void start(char input[], char text[], std::ifstream& ifs, double &randomiser, ch
 	std::cout << "[2]Continue Game?\n";
 	std::cout << "[3]Exit\n";
 	std::cin >> n;
+	std::cin.ignore();
 	while (n < 1 || n>3)
 	{
 		std::cout << "input must be an integer 1, 2 or 3\n";
@@ -344,7 +381,9 @@ void start(char input[], char text[], std::ifstream& ifs, double &randomiser, ch
 		break;
 	}
 	case 3:
-		return ;
+		break; ;
+	default:
+		break;
 	}
 	play(input, text, ifs, randomiser, corr, color, errors, words, wordsCount);
 }
@@ -353,15 +392,17 @@ void play(char input[], char text[], std::ifstream& ifs, double& randomiser, cha
 {
 	while (true)
 	{
-		printColored(text, color);
-		std::cout << "[0]To exit without saving\n";
-		std::cout << "[-1]To exit and save\n";
-		std::cout << "Enter the number of the word you wish to inspect\n";
+		system("cls");//
+		printColored(corr, color);
+		std::cout << "\n[0]To exit without saving";
+		std::cout << "\n[-1]To exit and save";
+		std::cout << "\nEnter the number of the word you wish to inspect: ";
 		int n = 0;
 		std::cin >> n;
-		while (n < -1 && n <= wordsCount)
+		std::cin.ignore();
+		while (n < -1 || n > wordsCount || !isPartOfWordCorr(color, words[2 * (n-1)], words[2 * (n-1) + 1]))
 		{
-			std::cout << "Input must be an integer -1 or above and must not exede the number of words\n";
+			std::cout << "Input must be an integer -1 or above and must not exede the number of words, the word must also be corrupted\n";
 			std::cin >> n;
 		}
 		if (n == 0)
@@ -377,42 +418,107 @@ void chosenWord(char input[], char text[], std::ifstream& ifs, double& randomise
 {
 	while (true)
 	{
-		int wordStart = 0, wordEnd = 0, n = 0,wordLenght;
-		if (isBlank(text[0]))
-		{
-			wordStart = words[wordNumber - 1] + 1;
-		}
-		else
-		{
-			wordStart = words[wordNumber - 1];
-		}
-			wordEnd = words[wordNumber ];
-		printColored(text, color);
+		system("cls");//C:/Users/Admin/Desktop/input.txt
+		int wordIndex = wordNumber - 1;
+		int wordStart = words[2 * wordIndex], wordEnd = words[2 * wordIndex + 1], n = 0, wordLenght = wordEnd-wordStart +1;
+		if (!isPartOfWordCorr(color, wordStart, wordEnd))
+			break;
+		printColored(corr, color);
 		std::cout << "\nSelected word is: ";
-		printColored(text, color);
-		std::cout << "[-1]To exit without saving\n";
+		printColored(corr, color, wordStart,wordEnd);
+		std::cout << "\n[-1]To exit without saving\n";
 		std::cout << "[-2]To exit and save\n";
 		std::cout << "Enter the number of the character in this word you wish to inspect (0 to cancel):\n";
-		int n = 0;
 		std::cin >> n;
-		while (n < -2 && n <= wordsCount)
+		int charIndex = words[2 * wordIndex] - 1;
+		while ((n < -2 || n > wordLenght )&&  (color[charIndex + n] != RED))
 		{
-			std::cout << "Input must be an integer -1 or above and must not exede the number of letters\n";
+			std::cout << "Input must be an integer less then word lenght as well as the letter MUST be corrupted\n";
 			std::cin >> n;
 		}
-		if (n == -1)
+		if (n == 0)
+			break;
+		else if (n == -1)
 			exitGame();
 		else if (n == -2)
 			saveGame();
-		else
-			chosenLetter(input, text, ifs, randomiser, corr, color, errors, words, wordsCount, n);
+		else 
+			chosenLetter(input, text, ifs, randomiser, corr, color, errors, words, wordsCount, wordNumber, n);
 	}
 }
 
 void chosenLetter(char input[], char text[], std::ifstream& ifs, double& randomiser, char corr[], int color[], int& errors, int words[], int& wordsCount, int wordNumber, int letterNumber)
 {
+	while (true)
+	{
+		system("cls");////C:/Users/Admin/Desktop/input.txt
+		int wordIndex = wordNumber - 1;
+		int wordStart = words[2 * wordIndex], wordEnd = words[2 * wordIndex + 1], n = 0, wordLenght = wordEnd - wordStart + 1;
+		if (!isPartOfWordCorr(color, wordStart, wordEnd))
+			break ;
+		printColored(corr, color);
+		std::cout << "\nSelected word is: ";
+		printColored(corr, color, wordStart, wordEnd);
+		std::cout << "\nSelected char is:";
+		for (int i = 0; i < letterNumber; i++)
+			std::cout << " ";
+		std::cout << "^\n";
+		std::cout << "\nChoose what to change the selected character to:\n";
+		int charIndex = words[2 * wordIndex] + letterNumber - 1;
+		char letter = corr[charIndex];
+		changeCharTo(letter);
+		std::cin >> n;
+		while (n < -2 || n > 6)
+		{
+			std::cout << "Input must be an integer -1 or above and must not exede the number of letters\n";
+			std::cin >> n;
+		}
+		if (n == 0)
+			return;
+		else if (n == -1)
+			exitGame();
+		else if (n == -2)
+			saveGame();
+		else
+		{
+			corr[charIndex] = invertAtPosition(letter, n - 1);
+			if (corr[charIndex] == text[charIndex])
+				color[charIndex] = GREEN;
+			else
+				errors++;
+			break;
+		}
+	}
+}
+
+bool isPartOfWordCorr(const int* color, int start, int end)
+{
+	for (int i = start; i <= end; i++)
+	{
+		if (color[i] == RED)
+			return true;
+	}
+	return false;
+}
+
+void changeCharTo(char sym)
+{
+	std::cout << "[0] Cancel\n";
+	std::cout << "[1]" << invertAtPosition(sym, 0) << "\n";
+	std::cout << "[2]" << invertAtPosition(sym, 1) << "\n";
+	std::cout << "[3]" << invertAtPosition(sym, 2) << "\n";
+	std::cout << "[4]" << invertAtPosition(sym, 3) << "\n";
+	std::cout << "[5]" << invertAtPosition(sym, 4) << "\n";
+	std::cout << "[6]" << invertAtPosition(sym, 5) << "\n";
+	std::cout << "Your choice:\n";
 }
 
 void saveGame()
 {
+}
+
+void exitGame()
+{
+	std::cout << "Thanks for playing!";
+	exit( 0);
 }
